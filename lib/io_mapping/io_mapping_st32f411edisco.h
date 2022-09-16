9289ef3571b15de55dd79670ff823f90f52f9846
sam-sam <sam-sam@meta-sammy>
2022-09-15 20:16:33 -0700

    Cleanup a bit

--- lib/io_mapping/io_mapping_st32f411edisco.h
+++ lib/io_mapping/io_mapping_st32f411edisco.h
@@ -1,7 +1,19 @@
 #pragma once
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32f4xx.h"
 #include <stm32f4xx_hal.h>
 
-#define LED_PIN                                GPIO_PIN_15
-#define LED_GPIO_PORT                          GPIOD
-#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOD_CLK_ENABLE()
+#define BLUE_LED_PIN                GPIO_PIN_15
+#define BLUE_LED_GPIO_PORT          GPIOD
+#define LED_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOD_CLK_ENABLE()
+
+#define BUTTON_PIN                  GPIO_PIN_0
+#define BUTTON_GPIO_PORT            GPIOA
+
+#ifdef __cplusplus
+ } // extern "C"
+#endif
--- lib/led/led.h
+++ lib/led/led.h
@@ -1,17 +1,30 @@
 #pragma once
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 #include <io_mapping.h>
 
 void LED_Init()
 {
+  // NOTE (from HAL): After reset, the peripheral clock (used for registers read/write access)
+  // is disabled and the application software has to enable this clock before using it.
   LED_GPIO_CLK_ENABLE();
+
+  // Setup the GPIO:
   GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Pin = BLUE_LED_PIN;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct);
+  HAL_GPIO_Init(BLUE_LED_GPIO_PORT, &GPIO_InitStruct);
 }
 
 void LEDBlink() {
-    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+    HAL_GPIO_TogglePin(BLUE_LED_GPIO_PORT, BLUE_LED_PIN);
 }
+
+#ifdef __cplusplus
+ } // extern "C"
+#endif
