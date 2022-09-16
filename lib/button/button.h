81a390f121ccda4e9859cf7a7aa24c1c70461bef
sam-sam <sam-sam@meta-sammy>
2022-09-15 21:16:40 -0700

    Basic polling button implementation

--- lib/button/button.h
+++ lib/button/button.h
@@ -2,34 +2,28 @@
 
 #include <stm32f4xx_hal.h>
 
-
-class LED {
+class Button {
   private:
     GPIO_TypeDef *m_port;
     uint32_t m_pin;
+    bool pressed;
   
   public:
-    LED(GPIO_TypeDef *port, uint32_t pin) :
+    Button(GPIO_TypeDef *port, uint32_t pin) :
       m_port(port), m_pin(pin)
     {
       // Setup the GPIO:
       GPIO_InitTypeDef GPIO_InitStruct;
       GPIO_InitStruct.Pin = m_pin;
-      GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+      GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
       GPIO_InitStruct.Pull = GPIO_PULLUP;
       GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
       HAL_GPIO_Init(m_port, &GPIO_InitStruct);
     }
 
-    void TurnOn() const {
-      HAL_GPIO_WritePin(m_port, m_pin, GPIO_PIN_SET);
-    }
-
-    void TurnOff() const {
-      HAL_GPIO_WritePin(m_port, m_pin, GPIO_PIN_RESET);
-    }
-
-    void Blink() const {
-      HAL_GPIO_TogglePin(m_port, m_pin);
+    bool IsPressed() const {
+      // Check state
+      GPIO_PinState state = HAL_GPIO_ReadPin(m_port, m_pin);
+      return (state == GPIO_PIN_SET);
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <led.h>
+#include <button.h>
 #include <io_mapping.h>
+#include <memory>
 
 enum LEDs {
   LED_BLUE,
@@ -8,6 +10,15 @@ enum LEDs {
   LED_RED,
   LED_MAX,
 };
+
+struct ProgramState {
+    std::unique_ptr<LED> leds[LED_MAX];
+    std::unique_ptr<Button> button;
+};
+
+
+const uint32_t BLINK_INTERVAL_MS = 1000;
+const uint32_t LOOP_INTERVAL_MS = 5; 
 
 
 int main(void)
@@ -18,18 +29,29 @@ int main(void)
   // is disabled and the application software has to enable this clock before using it.
   LED_GPIO_CLK_ENABLE();
 
-  LED leds[LED_MAX] = {
-    LED(LED_GPIO_PORT, BLUE_LED_PIN),
-    LED(LED_GPIO_PORT, GREEN_LED_PIN),
-    LED(LED_GPIO_PORT, ORANGE_LED_PIN),
-    LED(LED_GPIO_PORT, RED_LED_PIN),
-  };
+  ProgramState g_state;
+  g_state.leds[LED_BLUE] = std::make_unique<LED>(LED_GPIO_PORT, BLUE_LED_PIN);
+  g_state.leds[LED_GREEN] = std::make_unique<LED>(LED_GPIO_PORT, GREEN_LED_PIN);
+  g_state.leds[LED_ORANGE] = std::make_unique<LED>(LED_GPIO_PORT, ORANGE_LED_PIN);
+  g_state.leds[LED_RED] = std::make_unique<LED>(LED_GPIO_PORT, RED_LED_PIN);
+  g_state.button = std::make_unique<Button>(BUTTON_GPIO_PORT, BUTTON_PIN);
+
   int cur_led = LED_BLUE;
+  uint32_t blink_elapsed = 0;
 
   while (1)
   {
-    leds[cur_led].Blink();
-    HAL_Delay(1000);
+    if (g_state.button->IsPressed()) {
+      g_state.leds[cur_led]->TurnOff();
+      blink_elapsed = BLINK_INTERVAL_MS;
+    } else {
+      blink_elapsed += LOOP_INTERVAL_MS;
+      if (blink_elapsed >= BLINK_INTERVAL_MS) {
+        blink_elapsed = 0;
+        g_state.leds[cur_led]->Blink();
+      }
+    }
+    HAL_Delay(LOOP_INTERVAL_MS);
   }
 }
 
